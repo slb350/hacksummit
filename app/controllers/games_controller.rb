64e95097ba41455fe1f8b433056b0d148dd1cfa6
Matt Hull <matt.hull@lacunasolutionstn.com>
2014-06-14 17:28:03 -0700

    stubbed out example choice

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,17 +11,38 @@ class GamesController < ApplicationController
   end
 
   def create
-    choices = [
-
-    ]
-
     player = {}.tap do |p|
       p[:name] = params[:player_name]
       p[:color] = params[:player_color]
-      p[:pending_choices] = choices
+      p[:pending_choices] = standard_choices
       p[:completed_choices] = []
     end
 
     # now persist player
+  end
+
+  protected
+
+  def standard_choices
+    [
+      {
+        location: {
+          name: "San Francisco",
+          lat: "37.8",
+          long: "122.4"
+        },
+        description: "Your car is broken down WHAT DO YOU DO",
+        options: [
+          {
+            description: "Wait for help",
+            impact: {
+              food: -5,
+              gas: 10,
+              sea_level: 0.1
+            }
+          }
+        ]
+      }
+    ]
   end
 end
