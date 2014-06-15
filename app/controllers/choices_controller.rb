034072f74a4325187c334d22623fddc7be452795
Matt Hull <matt.hull@lacunasolutionstn.com>
2014-06-14 17:12:06 -0700

    faked data for resources and environmental impact

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,6 +1,14 @@
 class ChoicesController < ApplicationController
   def new
     @current_user = current_user
+
+    @environment = OpenStruct.new sea_level: 2,
+      temp: 80,
+      precip: 5
+
+    @resources = OpenStruct.new money: 200,
+      food: 3,
+      gas: 8
   end
 
   def create
