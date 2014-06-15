4ffbd9b8a2ab4ecdcbca091dcc015d579c158a2e
Stephen Brandon <brandon.steve@gmail.com>
2014-06-15 08:59:19 +0200

    fix deploy tasks and updated Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,23 @@ set :linked_dirs, %w[tmp/cache]
 
 namespace :deploy do
 
+desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
 
-  after :finishing
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
+  after :finishing, 'deploy:cleanup'
 
 end
