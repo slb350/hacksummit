85021258b2d85bd1c78e49a925fe4d21ddc85b95
Stephen Brandon <brandon.steve@gmail.com>
2014-06-15 09:32:59 +0200

    kill rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,7 +16,7 @@ namespace :deploy do
 desc 'Restart Rails Services'
   task :rails_restart do
     on roles(:all), in: :sequence, wait: 5 do
-      command "cd /home/#{fetch(:user)/rails.sh}"
+     execute "cd /home/#{fetch(:user)/; ./rails.sh}"
     end
   end
 
