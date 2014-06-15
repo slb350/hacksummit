2f84e36ea2dcf9fff77a7a683ba793e9cd71d10d
Stephen Brandon <brandon.steve@gmail.com>
2014-06-15 09:13:26 +0200

    added rails restart task

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,15 +20,6 @@ desc 'Restart Rails Services'
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
   after :finishing, 'deploy:rails_restart'
 
 end
